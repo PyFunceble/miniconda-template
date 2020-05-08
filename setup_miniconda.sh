3c01bc81cb7ce59ae341e0b8d11df186686123eb
spirillen <44526987+spirillen@users.noreply.github.com>
2020-05-08 18:17:37 +0200

    New url for download miniconda

--- setup_miniconda.sh
+++ setup_miniconda.sh
@@ -19,7 +19,7 @@ source .bashrc
 
 # 3. Download Conda
 wget -O miniconda.sh \
-  https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # 4. Install Conda
 bash miniconda.sh -b -p ${HOME}/miniconda
