aab0a6cee465681ddd2e23340884b4ac7c856823
Spirillen <pdns@protonmail.com>
2020-12-23 13:20:20 +0100

    Not finished, but working on x86_64 systems

--- setup_miniconda.sh
+++ setup_miniconda.sh
@@ -13,49 +13,44 @@
 # -------------------------------
 
 # Stop on any error
-set -ex
+set -e
 
 # Set python version
+# to find the latest version
+# `conda search --full-name python`
 pythonVersion="3.9.1"
 
 # Set conda install dir
 condaInstallDir="${HOME}/miniconda"
 
-arc="$(uname -m)"
-
-conda_source () {
-  if [ "${arc}" == x86_64]
-    then
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
-    
-  elif [ "${arc}" == arm7l]
-    then
-    if [ ! -d "${condaInstallDir}" ]
-    then
-      export PATH="${condaInstallDir}/bin:$PATH"
-      echo "conda install anaconda-client"
-      echo "'"export PATH="${condaInstallDir}/bin:$PATH""'" \
-        >> "~/.bash_aliases"
-      source "~/.bashrc"
-    else
-    export PATH="${condaInstallDir}/bin:$PATH"
-    fi
-  
-  fi
-}
-
-# Download and Install Conda
-if [ ! -d "${condaInstallDir}" ]
+if [[ ! -d ${condaInstallDir} ]]
+then
+  if [ $(uname -m) == 'x86_64' ]
   then
-      if [ ! -f "miniconda-${arc}.sh" ]
-        then
-        wget -O "miniconda-${arc}.sh" \
-          "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-${arc}.sh"
-      fi
-    bash "miniconda-${arc}.sh" -b -p "${condaInstallDir}"
+  # Download Conda
+  wget -O miniconda.sh \
+    'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+  
+  elif [ $(uname -m) == 'armv7l' ]
+    then
+    exec ./setup-arm7l.sh && exit 0
+
+  elif [ $(uname -m) == 'x86' ]
+    then
+    wget -O miniconda.sh \
+    'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh'
+
+  elif [ $(uname -m) == 'ppc64le' ]
+    then
+    wget -O miniconda.sh \
+    'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh'
+  fi
+
+  # Install Conda
+  bash miniconda.sh -b -p "${condaInstallDir}"
 
   # inspired by https://gitlab.com/my-privacy-dns/test-suites-and-drafts/OpenWPM/-/blob/master/scripts/install-miniconda.sh#L14-18
-  conda_source
+  source "$HOME/miniconda/etc/profile.d/conda.sh"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
@@ -63,7 +58,7 @@ if [ ! -d "${condaInstallDir}" ]
 else
   echo "We assume that conda is already installed."
   echo "We are updating the environment"
-  conda_source
+  source "$HOME/miniconda/etc/profile.d/conda.sh"
   hash -r
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
@@ -80,7 +75,7 @@ then
   pyfunceblePackageName="pyfunceble"
 
   # Get the conda CLI.
-  conda_source
+  source "${condaInstallDir}/etc/profile.d/conda.sh"
 
   hash conda
 
@@ -176,15 +171,7 @@ then
   pyfunceblePackageName="pyfunceble4"
 
   # Get the conda CLI.
-  if [ -f "${condaInstallDir}/etc/profile.d/conda.sh" ]
-    then
-    source "${condaInstallDir}/etc/profile.d/conda.sh"
-    elif [ $(grep -q -i 'conda' "$HOME/.bash_aliases") == '' ]
-      then
-      echo -e "'"\nexport PATH="${condaInstallDir}/bin:$PATH"\n"'" \
-        >> "$HOME/.bash_aliases"
-      export PATH="${condaInstallDir}/bin:$PATH"
-  fi
+  source "${condaInstallDir}/etc/profile.d/conda.sh"
 
   hash conda
 
